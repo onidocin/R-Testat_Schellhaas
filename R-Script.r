069ae01f1127a0a2d756434294896ab9e89b6d0f
onidocin <onidocin@gmail.com>
2025-12-09 10:16:24 +0100

    Tipps von Glahn hinzugefügt

--- R-Script.r
+++ R-Script.r
@@ -26,16 +26,18 @@ View(tabellen_beide)
 # 2. Erzeugen Sie je einen Sekundärindex für die Kantone und einen für die Bezirke, so dass für jeden Gemeindendatensatz auch der Kantons- und Bezirksname ersichtlich ist.
 tabellen_beide |>
   group_by()
-
+mutate()
 
 # 3. Organisieren Sie die Wohnbevölkerung nach den folgenden Altersgruppen: 
 # Kinder (Bis 12 Jahre)
 # Minderjährige (unter 18 Jährige)
 # Erwachsene, jünger als 65 Jahre
 # Erwachsene, ab 65 Jahren
-
+reframe()
 # 4. Bestimmen Sie die statistischen Kennwerte für die drei Altersgruppen über alle Datensätze der Jahre 2010 und 2022 der Originalquelle und speichern Sie Ihre Ergebnisse als Tabelle.
+#Achtung: 
 
 # 5. Ermitteln Sie den Unterschied des Durchschnittsalters dieser Gruppen in den beiden Referenzjahren für alle Bezirke im Kanton Zürich und speichern Sie diese Unterschiede als separate Tabelle.
-
+write.csv2()
 # 6. Erstellen Sie einen Boxplot, aus dem die Verteilungen des Durchschnittsalters der Minderjährigen in den Gemeinden der 11 Bezirke des Kanton Zürichs ohne die Stadt Zürich in den beiden Jahren hervorgehen.
+# Daten Visualisieren
